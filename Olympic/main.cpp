095b2272efa6c4ca634ad8b3ba058920cc2c0f6a
voidespy <48234494+voidespy@users.noreply.github.com>
2023-12-05 14:14:32 -0500

    changed favicon href (maybe this needs to be in static too?)

--- Olympic/main.cpp
+++ Olympic/main.cpp
@@ -1,22 +1,56 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <filesystem>
+#include <random>
+
+namespace fs = std::filesystem;
+
+// Function to verify the integrity of all project files
+bool verifyIntegrity() {
+    // Logic to check the integrity of files
+    // Could involve checksums, file sizes, expected names, etc.
+    // ...
+
+    return true; // Return true if all files are verified, false otherwise
+}
+
+// Function to generate a unique username and password
+std::pair<std::string, std::string> generateCredentials() {
+    // Use a random number generator to create credentials
+    // ...
+
+    return {"RandomUsername", "RandomPassword"};
+}
 
 // Function to generate initial game state
 void generateInitialState() {
     // Load the model and start generating
-    // Will need to start another py to do that. will need to use a small model size
-    // Call AI service or perform some generation logic
+    // Will need to start another Python process to do that, will need to use a small model size
+    // ...
+
     // Save the state to a file or database
+    // ...
 }
 
 // Function to delete the game state
 void deleteState() {
     // Remove the state file or clear the database
+    // ...
 }
 
-
 int main() {
+    // Verify Integrity of all project files
+    if (!verifyIntegrity()) {
+        std::cout << "File verification failed." << std::endl;
+        return 1;
+    }
+
+    // Generate and serialize username and password
+    auto [username, password] = generateCredentials();
+    // Save credentials to a file or database
+    // ...
 
     // Check if state exists, if not, generate a new state
     generateInitialState();
